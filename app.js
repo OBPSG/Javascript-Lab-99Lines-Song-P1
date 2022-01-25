f73bb26f417b17e1381c4888973ea4375fc45802
OBPSG <vbeasupergamer@gmail.com>
2022-01-25 10:16:15 -0600

    Implemented and tested 99 Lines Song function

---
+++ app.js
@@ -0,0 +1,33 @@
+var friends = ["John", "Jane", "Mark", "Katie", "Smith"];
+
+function Sing99LinesSong(singer) {
+  for (let i = 99; i > 0; i--) {
+    if (i == 1) {
+      console.log(
+          "Only 1 line of code in the file, only more line of code; " +
+          singer +
+          " strikes one out, clears it all out, no more lines of code in the file!"
+      );
+    }
+    else if (i == 2) {
+      console.log(
+          "2 lines of code in the file, 2 lines of code; " +
+          singer +
+          " strikes one out, clears it all out, only one line of code in the file!"
+      );
+    } else {
+        console.log(
+            i +
+            " lines of code in the file, " +
+            i +
+            " lines of code; " +
+            singer +
+            " strikes one out, clears it all out, " +
+            (i - 1) +
+            " lines of code in the file!"
+        );
+    }
+  }
+}
+
+Sing99LinesSong("Andrew");
