46cb0d43762f0e04e20a17b622ec5a3219b1d0d0
OBPSG <vbeasupergamer@gmail.com>
2022-01-27 09:17:18 -0600

    Added readme, fixed typos, and tweaked some styling

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 var friends = ["John", "Jane", "Mark", "Katie", "Smith"];
 
+//Function that outputs the chorus for the 99 Lines of Code Song for a single friend
 function Sing99LinesSong(singer) {
   for (let i = 99; i > 0; i--) {
     if (i == 1) {
       console.log(
-          "Only 1 line of code in the file, only more line of code; " +
+        "Only 1 line of code in the file, only 1 more line of code; " +
           singer +
           " strikes one out, clears it all out, no more lines of code in the file!"
       );
-    }
-    else if (i == 2) {
+    } else if (i == 2) {
       console.log(
-          "2 lines of code in the file, 2 lines of code; " +
+        "2 lines of code in the file, 2 lines of code; " +
           singer +
-          " strikes one out, clears it all out, only one line of code in the file!"
+          " strikes one out, clears it all out, only 1 line of code in the file!"
       );
     } else {
-        console.log(
-            i +
-            " lines of code in the file, " +
-            i +
-            " lines of code; " +
-            singer +
-            " strikes one out, clears it all out, " +
-            (i - 1) +
-            " lines of code in the file!"
-        );
+      console.log(
+        i +
+          " lines of code in the file, " +
+          i +
+          " lines of code; " +
+          singer +
+          " strikes one out, clears it all out, " +
+          (i - 1) +
+          " lines of code in the file!"
+      );
     }
   }
 }
 
-//Sing99LinesSong("Andrew");
-for(let i = 0; i < friends.length; i++)
-{
+//Loop over the friends array calling the Sing99LinesSong function
+for (let i = 0; i < friends.length; i++) {
   console.log(friends[i].toUpperCase() + ":");
   Sing99LinesSong(friends[i]);
 }
